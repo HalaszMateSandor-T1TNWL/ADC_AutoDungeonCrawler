639c4c1630d6ec22632ce24130bef56167649182
BMartiin <baloghmartin.prs@gmail.com>
2026-02-20 11:15:42 +0100

    Teszt push

---
+++ NewScript.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+
+public partial class PlayerTest : CharacterBody2D
+{
+    // Mozgási sebesség beállítása
+    [Export]
+    public float Speed = 300.0f;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector2 velocity = Velocity;
+
+        // Bemenetek lekérése (alapértelmezett ui_ irányok)
+        Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+
+        if (direction != Vector2.Zero)
+        {
+            velocity = direction * Speed;
+        }
+        else
+        {
+            velocity = velocity.MoveToward(Vector2.Zero, Speed);
+        }
+
+        Velocity = velocity;
+        MoveAndSlide();
+
+        // Teszt funkció: Ha megnyomod a Space-t, írjon ki valamit a konzolra
+        if (Input.IsActionJustPressed("ui_accept"))
+        {
+            GD.Print("Teszt: Interakció történt! A játékos pozíciója: " + GlobalPosition);
+            PerformQuickTest();
+        }
+    }
+
+    private void PerformQuickTest()
+    {
+        // Egy egyszerű logikai ellenőrzés példa
+        GD.Print("C# Script futtatása sikeres a Godot-ban.");
+    }
+}
