060c0acdadb53f15a4b3f203559a176bc6da333b
HalaszMateSandor-T1TNWL <maseny0@gmail.com>
2026-03-26 15:00:18 +0100

    Finished implementation of the Pathfinding algorithm for both the Enemy and Player side

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -1,44 +1,24 @@
 using Godot;
 using System;
 
-public partial class Enemy : Area2D
+public partial class Enemy : Entity
 {
 	[Signal] public delegate void HPChangedEventHandler(float currentHP);
 	
 	public float MaxHealth = 100.0f;
-
-	public float CurrentHealth;
-
-	public float damage;
 	
-	private TileMapLayer _tileMap;
-	private AStarGrid2D _astar;
-
 	public override void _Ready()
 	{
-		_tileMap = GetNode<TileMapLayer>($"../TileMapLayer");
-
-		_astar = new AStarGrid2D();
-		_astar.Region = _tileMap.GetUsedRect();
-		_astar.CellSize = new Vector2I(32, 32);
-		_astar.DiagonalMode = AStarGrid2D.DiagonalModeEnum.Never;
-		_astar.Update();
-
-		if(IsSpawnValid())
-		{
-			QueueFree();
-		}
+		AddToGroup("enemy");
 
 		HPChange(5);
 	}
-	
-	public bool IsSpawnValid()
-	{
-		return _astar.Region.HasPoint(_tileMap.LocalToMap(this.GlobalPosition)) == false || 
-			_tileMap.GetCellTileData(_tileMap.LocalToMap(this.GlobalPosition)) == null || 
-			(bool)_tileMap.GetCellTileData(_tileMap.LocalToMap(this.GlobalPosition)).GetCustomData("Walkable") == false;
-	}
-	
+
+	public override void OnQueueForFree()
+    {
+        this.QueueFree();
+    }
+
 	public void HPChange(float change)
 	{
 		CurrentHealth = MaxHealth;
@@ -54,11 +34,11 @@ public partial class Enemy : Area2D
 	
 	public void DamageTaken(float damage)
 	{
-			TakeDamage(damage);
-			if(CurrentHealth <= 0)
-			{
-				QueueFree();
-			}
+		TakeDamage(damage);
+		if(CurrentHealth <= 0)
+		{
+			QueueFree();
+		}
 	}
 
 	public void HealthGained(float amount)
@@ -76,5 +56,4 @@ public partial class Enemy : Area2D
 		CurrentHealth += amount;
 		CurrentHealth = Mathf.Clamp(CurrentHealth, 0, MaxHealth);
 	}
-
 }
--- Scripts/Seeker.cs
+++ Scripts/Seeker.cs
@@ -6,30 +6,29 @@ using System.Linq;
 public partial class Seeker : Entity
 {
 	[Export] public new float movementSpeed = 100.0f;
-	[Export] public new int attackRange = 0;
+	//[Export] public new int attackRange = 0;
 	[Export] public new float maxHealth = 100.0f;
 	[Export] public new float CurrentHealth = 100.0f;
 	[Export] public new float damage = 2.0f;
-
 
 	[Signal] public delegate void DealDamageEventHandler(float damage);
 	
 	
 	public override void _Ready()
 	{
-		
+		AddToGroup("player");
 	}
 
-	public void OnQueueForFree()
+	public override void OnQueueForFree()
 	{
 		this.QueueFree();
 	}
 
-	public void OnAreaEntered(Area2D area)
+	public void OnBodyEntered(Node2D body)
 	{
-		if(area.IsInGroup("enemy"))
+		if(body.IsInGroup("enemy"))
 		{
-			GD.Print(area.GlobalPosition);
+			GD.Print(body.GlobalPosition);
 		}
 	}
 }
